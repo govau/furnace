a6dbf84c9c48dd4e6710f4b7c48ea72d978664d0
Alex Page <alex@alexpage.com.au>
2017-11-15 16:13:21 +1100

    Allowing slack to fail silently

--- src/slack.js
+++ src/slack.js
@@ -17,7 +17,7 @@ import { Log }                from './helper';
 
 
 const IncomingWebhook = require('@slack/client').IncomingWebhook;
-const URL             = process.env.SLACK_WEBHOOK || '';
+const URL             = process.env.SLACK_WEBHOOK;
 const Webhook         = new IncomingWebhook( URL );
 
 
@@ -26,53 +26,58 @@ export const SlackMessage = ( messageData ) => {
 
 	return new Promise( ( resolve, reject ) => {
 
-		let message = {
-			text: `*Furnace*:\n\n`,
-			attachments: [{
-				'fallback': `_Inspect the GOLD nuggets?_`,
-				'pretext': `_Inspect the GOLD nuggets?_`,
-				'color': `#ff4500`,
-				'mrkdwn_in': [
-					`text`,
-					`pretext`,
-					`fields`,
-				],
-				'fields': [
-					{
-						'title': `Components`,
-						'value': '' +
-							'_Selected_: `' + messageData.components.length + '`\n' +
-							'_Modules_: `' + messageData.components.join( '` , `') + '`\n\n\n',
-						'short': false,
-					},
-					{
-						'title': `Options`,
-						'value': '' +
-							'_JS Output_: `' + messageData.jsOutput + '`\n' +
-							'_Style Output_: `' + messageData.styleOutput + '`\n\n\n',
-						'short': false,
-					},
-					{
-						'title': `Client`,
-						'value': '' +
-							'_IP_: `' + messageData.ip + '`',
-						'short': false,
-					}
-				],
-				'footer': `:gold:`,
-			}]
+		if( URL && URL.length > 0 ) {
+			const message = {
+				text: `*Furnace*:\n\n`,
+				attachments: [{
+					'fallback': `_Inspect the GOLD nuggets?_`,
+					'pretext': `_Inspect the GOLD nuggets?_`,
+					'color': `#ff4500`,
+					'mrkdwn_in': [
+						`text`,
+						`pretext`,
+						`fields`,
+					],
+					'fields': [
+						{
+							'title': `Components`,
+							'value': '' +
+								'_Selected_: `' + messageData.components.length + '`\n' +
+								'_Modules_: `' + messageData.components.join( '` , `') + '`\n\n\n',
+							'short': false,
+						},
+						{
+							'title': `Options`,
+							'value': '' +
+								'_JS Output_: `' + messageData.jsOutput + '`\n' +
+								'_Style Output_: `' + messageData.styleOutput + '`\n\n\n',
+							'short': false,
+						},
+						{
+							'title': `Client`,
+							'value': '' +
+								'_IP_: `' + messageData.ip + '`',
+							'short': false,
+						}
+					],
+					'footer': `:gold:`,
+				}]
+			}
+
+			Webhook.send( message, ( error, header, statusCode, body ) => {
+				if( error ) {
+					reject( error );
+				}
+				else {
+					Log.verbose(`Slack received: ${ statusCode }`);
+					Log.verbose(`Slack message sent:\n ${ message }`);
+					resolve();
+				}
+			});
 		}
-
-		Webhook.send( message, ( error, header, statusCode, body ) => {
-			if( error ) {
-				reject( error );
-			}
-			else {
-				Log.verbose(`Slack received: ${ statusCode }`);
-				Log.verbose(`Slack message sent:\n ${ message }`);
-				resolve();
-			}
-		});
-
+		else {
+			Log.message( `Failed to send slack message, SLACK_WEBHOOK (${ URL }) environment variable not found.` );
+			resolve();
+		}
 	})
 }
