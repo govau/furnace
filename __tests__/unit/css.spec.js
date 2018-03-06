1fc7fcc4199afa6dad5763f6943d6fc58d505f64
Alex Page <alex@alexpage.com.au>
2018-03-06 13:39:48 +1100

    Fixing spacing and comments

--- __tests__/unit/css.spec.js
+++ __tests__/unit/css.spec.js
@@ -17,10 +17,10 @@
 import { GetMinCss, Autoprefix } from '../../src/css';
 
 
-// ----------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
 // GetMinCss
-// ----------------------------------------------------------------
-test('GetMinCss: should minify and autoprefix a scss string', () => {
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+test( 'GetMinCss: should minify and autoprefix a scss string', () => {
 	const sass = `$red: red; .card{ background-color: $red; box-shadow: 10px 10px 10px #000; }`;
 	const css = `.card{background-color:red;-webkit-box-shadow:10px 10px 10px #000;box-shadow:10px 10px 10px #000}\n`;
 
@@ -29,17 +29,17 @@ test('GetMinCss: should minify and autoprefix a scss string', () => {
 });
 
 
-test('GetMinCss: should throw an error', () => {
+test( 'GetMinCss: should throw an error', () => {
 
 	GetMinCss( 'asasasdad' )
 		.catch( error => expect( error.message ).toBe( `Invalid CSS after "asasasdad": expected "{", was ""` ) );
 });
 
 
-// ----------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Autoprefixer
-// ----------------------------------------------------------------
-test('GetMinCSS: should throw a warning for invalid autoprefix', () => {
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+test( 'GetMinCSS: should throw a warning for invalid autoprefix', () => {
 	console.warn = jest.fn();
 	const sass = 'body { display: box; }';
 	const warning = `autoprefixer: <css input>:1:6: You should write display: flex by final spec instead of display: box`;
@@ -52,7 +52,7 @@ test('GetMinCSS: should throw a warning for invalid autoprefix', () => {
 });
 
 
-test('Autoprefixer: should throw a warning', () => {
+test( 'Autoprefixer: should throw a warning', () => {
 	Autoprefix( 'asasasdad' )
 		.catch( error => error => expect( error.message ).toBe( `<css input>:1:1: Unknown word` ) );
 });
